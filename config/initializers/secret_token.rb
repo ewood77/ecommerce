13f37986ba3e9d65fb719e8275609586134031a2
Eric A. Wood <eric.wood.ea@gmail.com>
2013-11-08 08:57:43 -0600

    Initial git push for ecommerce website.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecommerce::Application.config.secret_token = 'd12834792490a8e22c3b59572edd04fd4c6d6743760e493039ed47c5d1cb307672748b772e0515cb9f1ba72be86282b9cbe83a3de5758440b9968613684feb1c'
